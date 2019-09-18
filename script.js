c4b97c37866c8ab7ed82b4d631899c19d31ec9a7
Vlados <vlados64_90@mail.ru>
2019-09-18 20:10:15 +0300

    abcde

--- script.js
+++ script.js
@@ -21,22 +21,24 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
+    savings: true,
+    chooseExpenses: {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+                b = prompt ("Во сколько обойдется?", "");
+        
+            if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+                appData.expenses[a] = b;
+            } else {
+                i--;
+            }
+        }
+    }
 };
 
 
 function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
-            b = prompt ("Во сколько обойдется?", "");
-    
-        if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
-            appData.expenses[a] = b;
-        } else {
-            i--;
-        }
-    
-    };
+    ;
 }
 chooseExpenses();
 
